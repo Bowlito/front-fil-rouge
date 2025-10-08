e23aab12ecff77950a9d324ad47615c2577d5b2b
Bowlito <julien.catoio@gmail.com>
2025-10-08 15:54:59 +0200

    ajout / affichage / suppression coms

--- src/components/Commentaires/Commentaires.jsx
+++ src/components/Commentaires/Commentaires.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "../../../config/Axios_config/axios.config.js";
 import { formatDateTime } from "../../../config/date.utils.js";
 import "./Commentaire.css";
+import { GlobalContext } from "../../contexts/GlobalContext.jsx";
 
 export default function Commentaires({ postId }) {
+
+    const { user } = useContext(GlobalContext)
+    const [erreur, setErreur] = useState("");
 
     const [commentaires, setCommentaires] = useState([]);
 
@@ -12,35 +16,61 @@ export default function Commentaires({ postId }) {
 
         axios
             .get(`/publications/${postId}/commentaires`)
-            .then(res => setCommentaires(res.data))
+            .then(res => 
+                setCommentaires(res.data)
+                
+                
+            )
             .catch(err => {
                 console.error("Erreur lors du chargement des commentaires :", err);
                 setCommentaires([]);
             });
-    }, [postId]);
+    }, [postId, commentaires]);
+
+    function supprimerCom(comId){
+        axios
+            .delete(`/commentaires/${comId}`)
+            .then(()=>{
+                setCommentaires(coms => coms.filter((c) => c.id_com !== comId))
+                console.log("Commentaire supprimé avec succès")
+            })
+            .catch((erreur) => {
+                console.error("Erreur de suppression :", erreur)
+                setErreur("Impossible de supprimer ce commentaire pour le moment.");
+            });
+    }
 
     return (
         <div>
             <h2>Les commentaires</h2>
+
             <div className="card comments-card shadow-sm mt-4 p-3">
                 <h5 className="mb-3">Commentaires</h5>
                 <ul className="list-group list-group-flush">
                     {commentaires.length > 0 ? (
                         commentaires.map((c, ind) => (
+
+
                             <li key={ind} className="list-group-item">
                                 <div className="d-flex justify-content-between">
-                                    <strong>{c.nom} {c.prenom }</strong>
-                                    <small className="text-muted">{formatDateTime(c.created_at)}</small>
+                                    <strong>{c.nom} {c.prenom}</strong>
+                                    <small className="text-warning">{formatDateTime(c.created_at)}</small>
                                 </div>
                                 <p className="mb-0">{c.corps || "Aucun contenu"}</p>
+                                {(user?.id == c?.id_users|| user?.role == "admin") &&
+                                    <button onClick={() => supprimerCom(c.id_com)}>Supprimer</button>
+                                }
                             </li>
+
                         ))
                     ) : (
                         <li className="list-group-item text-light">
                             Aucun commentaire pour ce post.
                         </li>
+
                     )}
                 </ul>
+
             </div>
         </div>
     )
--- src/views/Post/Post.jsx
+++ src/views/Post/Post.jsx
@@ -5,41 +5,70 @@ import './Post.css';
 import { formatDateTime } from "../../../config/date.utils.js";
 import { GlobalContext } from "../../contexts/GlobalContext.jsx";
 import Commentaires from "../../components/Commentaires/Commentaires.jsx";
+import { useForm } from "react-hook-form";
 
 export default function Post() {
 
+    const { handleSubmit, register, formState: { errors, isValid, isSubmitSuccessful }, reset, } = useForm({
+        // resolver: yupResolver(userSchema),
+        mode: "onChange",
+    }
+    );
+
+    const {user} = useContext(GlobalContext)
     const { id } = useParams();
     const [post, setPost] = useState({});
     const [auteur, setAuteur] = useState({})
     const { isAuthenticated } = useContext(GlobalContext);
     const navigate = useNavigate();
-
+    
 
 
     useEffect(() => {
-    axios.get(`/publications/${id}`)
-        .then(res => {
-            setPost(res.data);
-            return axios.get(`/users/${res.data.id_users}`);
-        })
-        .then(resUser => setAuteur(resUser.data))
-        .catch(err => console.error(err));
-}, [id]);
-   
- 
+        axios.get(`/publications/${id}`)
+            .then(res => {
+                setPost(res.data);
+                return axios.get(`/users/${res.data.id_users}`);
+            })
+            .then(resUser => setAuteur(resUser.data))
+            .catch(err => console.error(err));
+    }, []);
+
+
     useEffect(() => {
         if (!isAuthenticated) {
             navigate('/connexion');
         }
     }, [isAuthenticated, navigate]);
 
+
+
+    function commenter(formData) {
+
+        formData.id_users = user.id
+        formData.id_publication = id
+        axios
+            .post('/commentaires', formData)
+            .then(res => {
+                console.log(user);
+                
+                console.log("commentaire ajouté");
+                console.log(res.data);
+                
+            
+                
+                
+            })
+            reset()
+    }
+
     return (
         <div>
             <div className="container my-5 post-container">
                 <div className="card post-card shadow-sm w-100" style={{ minWidth: "60%" }}>
-                    <div className="mb-2">Publié par <span style={{color: "orange", fontSize: "larger"}}>{auteur.nom} {auteur.prenom} </span></div>
+                    <div className="mb-2">Publié par <span style={{ color: "orange", fontSize: "larger" }}> {auteur.nom} {auteur.prenom} </span></div>
                     <img
-                        src={ post.chemin_image || "/thumbnail_image0.jpg"}
+                        src={post.chemin_image || "/thumbnail_image0.jpg"}
                         className="card-img-top post-image"
                         alt={post.titre}
                     />
@@ -52,21 +81,26 @@ export default function Post() {
                     </div>
                 </div>
 
-                
+
                 <div className="card comment-form-card shadow-sm mt-4 p-3">
                     <h5 className="mb-3">Ajouter un commentaire</h5>
-                    <div className="d-flex gap-2">
+
+                    <form className="d-flex gap-2" onSubmit={handleSubmit(commenter)}>
                         <textarea
                             className="form-control"
                             placeholder="Écrivez votre commentaire..."
                             rows="3"
-                        ></textarea>
+                            required
+                            {...register("corps")}></textarea>
                         <button className="btn btn-primary">Envoyer</button>
-                    </div>
+                    </form>
+
                 </div>
 
-                
+
                 <Commentaires postId={id} />
+                
+
             </div>
         </div>
     )
